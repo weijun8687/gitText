08a935e0e8f7b8d6c06eec3f5f936fab0689b4a9
Ibokan <ibokan@ibokandemac-mini-5.local>
2015-12-07 10:57:42 +0800

    Initial Commit

---
+++ 绘画/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  绘画
+//
+//  Created by Ibokan on 15/12/7.
+//  Copyright © 2015年 魏国军. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 绘画/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  绘画
+//
+//  Created by Ibokan on 15/12/7.
+//  Copyright © 2015年 魏国军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
